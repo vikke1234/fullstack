f552f7ca11122f1ee9cfe887e2eb3fd97837f598
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2019-02-04 16:14:29 +0200

    osa 2 a)

--- osa2/src/index.js
+++ osa2/src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import Course from './courses.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack -sovelluskehitys",
+      id: 1,
+      parts: [
+        {
+          name: "Reactin perusteet",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Tiedonvälitys propseilla",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "Komponenttien tila",
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewaret",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <Course courses={courses} />
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById("root"))
