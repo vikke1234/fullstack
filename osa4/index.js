a0e1cfb025ec8fc6f3e4b7ed573c14c78efe5bf7
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2019-02-15 13:56:09 +0200

    4.1 - 4.2

--- osa4/index.js
+++ osa4/index.js
@@ -1,44 +1,22 @@
 const http = require('http')
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const config = require('./utils/config')
+const router = require('./controllers/controllers')
+const app = express()
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://admin:admin@full-stack-shard-00-00-e52u1.mongodb.net:27017,full-stack-shard-00-01-e52u1.mongodb.net:27017,full-stack-shard-00-02-e52u1.mongodb.net:27017/test?ssl=true&replicaSet=full-stack-shard-0&authSource=admin&retryWrites=true'
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
+.then(() => console.log('connected to mongoDB'))
+.catch(error => console.log('error connecting to mongoDB: ', error.message))
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', router)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
---
+++ osa4/tests/list_helper.test.js
@@ -0,0 +1,1 @@
+const listHelper = require('./list_helper')
