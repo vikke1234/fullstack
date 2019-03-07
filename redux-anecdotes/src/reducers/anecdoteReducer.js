e3fdab89b9b44dfd087cb8b24a5eac3d7790fdab
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2019-03-07 09:05:45 +0200

    anecdotes

---
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,71 @@
+const anecdotesAtStart = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = anecdote => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+export const voteAnecdote = id => {
+  return {
+    type: "VOTE",
+    data: { id }
+  }
+}
+
+export const createAnecdote = content => {
+  return {
+    type: "CREATE",
+    data: { content: content, id: getId(), votes: 0 }
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  console.log("state now: ", state)
+  console.log("action", action)
+
+  switch (action.type) {
+    case "VOTE":
+      const id = action.data.id
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
+
+      console.log(anecdoteToChange)
+
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      state = state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+      break
+    case "CREATE":
+      state = state.concat(action.data)
+      break
+    default:
+      break
+  }
+  return state.sort((a1, a2) => {
+    if (a1.votes > a2.votes) {
+      return -1
+    } else if (a1.votes < a2.votes) {
+      return 1
+    }
+    return 0
+  })
+}
+
+export default reducer
